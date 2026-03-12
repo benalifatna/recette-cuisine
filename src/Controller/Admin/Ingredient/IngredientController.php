67fde2ac081a0ab78c07ff6797e1ddb2573c6269
Fatna <benalifatna02@gmail.com>
2026-03-13 00:24:31 +0100

    feat: L'administrateur peut consulter et modifier les ingrédients

--- src/Controller/Admin/Ingredient/IngredientController.php
+++ src/Controller/Admin/Ingredient/IngredientController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin\Ingredient;
 
 use App\Entity\Ingredient;
 use App\Form\Admin\IngredientFormType;
+use App\Repository\IngredientRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -15,14 +16,18 @@ final class IngredientController extends AbstractController
 {
     public function __construct(
         private readonly EntityManagerInterface $entityManager,
-        // private readonly IngredientRepository $tagRepository,
+        private readonly IngredientRepository $ingredientRepository,
     ) {
     }
 
     #[Route('/ingredient/list', name: 'app_admin_ingredient_index', methods: ['GET'])]
     public function index(): Response
     {
-        return $this->render('pages/admin/ingredient/index.html.twig');
+        $ingredients = $this->ingredientRepository->findAll();
+
+        return $this->render('pages/admin/ingredient/index.html.twig', [
+            'ingredients' => $ingredients,
+        ]);
     }
 
     #[Route('/ingredient/create', name: 'app_admin_ingredient_create', methods: ['GET', 'POST'])]
@@ -46,6 +51,30 @@ final class IngredientController extends AbstractController
         }
 
         return $this->render('pages/admin/ingredient/create.html.twig', [
+            'ingredientForm' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/ingredient/{id<\d+>}/edit', name: 'app_admin_ingredient_edit', methods: ['GET', 'POST'])]
+    public function edit(Ingredient $ingredient, Request $request): Response
+    {
+        // création du formulaire
+        $form = $this->createForm(IngredientFormType::class, $ingredient);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $ingredient->setUpdatedAt(new \DateTimeImmutable());
+
+            $this->entityManager->persist($ingredient);
+            $this->entityManager->flush();
+
+            $this->addFlash('success', 'L\'ingrédient a été modifié.');
+
+            return $this->redirectToRoute('app_admin_ingredient_index');
+        }
+
+        return $this->render('pages/admin/ingredient/edit.html.twig', [
+            'ingredient' => $ingredient,
             'ingredientForm' => $form->createView(),
         ]);
     }
